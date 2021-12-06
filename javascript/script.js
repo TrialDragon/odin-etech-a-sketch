27f4c91ffec1726f8c54713703bb3349169efad8
TrialDragon <cj.2003@live.com>
2021-12-05 21:05:16 -0800

    add grid reset button
    needed a way to reset the grid from it's hovered state

--- javascript/script.js
+++ javascript/script.js
@@ -6,10 +6,23 @@ for(let loop = 0; loop < 16; ++loop)
 	{
 		let cubeDiv = document.createElement('div');
 		cubeDiv.classList.add('cube');
-		
+		cubeDiv.addEventListener('mouseover', e =>
+			{
+				e.target.classList.add('hovered');
+			})
 		gridContainer.appendChild(cubeDiv);
 	}
 }
+
+const resetButton = document.querySelector('button.reset');
+resetButton.addEventListener('click', e =>
+	{
+		let cubes = gridContainer.children;
+		for(let loop = 0; loop < cubes.length; ++loop)
+		{
+			cubes[loop].classList.remove('hovered');
+		}
+	})
 
 const body = document.querySelector('body');
 body.appendChild(gridContainer);
